0e7033c3bacae4d622d15746279f4e4f6c3b497c
Gareth Solbeck <gsolbeck@gmail.com>
2015-06-04 16:24:41 -0400

    Add pop-in animation for new level icons
    Also add a "start here" sign to the first level that uses the same
    animation to appear.
    This code was written by Max Seidman.

--- src/Smorball/wwwroot/js/actors/map-level.js
+++ src/Smorball/wwwroot/js/actors/map-level.js
@@ -30,21 +30,56 @@ var MapLevel = (function (_super) {
         this.lock.x = 58;
         this.lock.y = -80;
         this.addChild(this.lock);
+        // Add the sign
+        this.sign = new createjs.Bitmap(smorball.resources.getResource("start_here_banner"));
+        this.sign.regX = 50;
+        this.sign.regY = 50;
+        this.sign.y = -100;
+        this.sign.visible = false;
+        this.addChild(this.sign);
         // Add the logo
         //this.logo = new createjs.Bitmap(smorball.resources.getResource("eugene_melonballers_logo_small"));
         this.updateLockedState();
         this.on("click", function (e) { return _this.onClick(); }, this, false, null, true);
     }
     MapLevel.prototype.updateLockedState = function () {
+        var wasUnlocked = this.isUnlocked;
         this.isUnlocked = smorball.user.hasUnlockedLevel(this.levelId);
+        if (!smorball.user.hasUnlockedLevel(this.levelId+1) && this.isUnlocked)
+        {
+            console.log("popping up stadium");
+            this.popupIcon();
+            this.popup=true;
+            if (this.levelId == 0) {
+                console.log("popping up sign");
+                this.popupSign();
+            }
+        } else {
+            this.sign.visible = false;
+        }
         if (this.isUnlocked) {
             this.lock.visible = false;
             this.stadium.visible = true;
-        }
-        else {
+        } else {
             this.lock.visible = true;
             this.stadium.visible = false;
         }
+    };
+    MapLevel.prototype.popupIcon = function () {
+        this.stadium.visible = true;
+        this.stadium.scaleX = 0;
+        this.stadium.scaleY = 0;
+        this.base.visible = true;
+        this.base.scaleX = 0;
+        this.base.scaleY = 0;
+        createjs.Tween.get(this.stadium).to({ scaleX: 1, scaleY: 1 }, 2000, createjs.Ease.elasticOut);
+        createjs.Tween.get(this.base).to({ scaleX: 1, scaleY: 1 }, 2000, createjs.Ease.elasticOut);
+    };
+    MapLevel.prototype.popupSign = function () {
+        this.sign.visible = true;
+        this.sign.scaleX = 0;
+        this.sign.scaleY = 0;
+        createjs.Tween.get(this.sign).to({ scaleX: 1, scaleY: 1 }, 2000, createjs.Ease.elasticOut);
     };
     MapLevel.prototype.onClick = function () {
         if (this.isUnlocked) {
